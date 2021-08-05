a0dcceb1b9feb7092954cf908dc7efcd4ef9a8d7
David Deket <david.deket@fh-campuswien.ac.at>
2021-08-05 14:34:57 +0200

    attributes added to gnfts

--- contracts/Gnft.sol
+++ contracts/Gnft.sol
@@ -8,9 +8,17 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 contract Gnft is ERC721Full {
 
     string[] public svgs;
+    uint256[] public strengths;
+    uint256[] public costs;
+    uint256[] public types;
+    uint256[] public rarities;
     mapping(string => bool) _svgExists;
     
     constructor() public ERC721Full("GamingNFT", "GNFT") {}
+    
+    function random() internal view returns (uint) {
+        return uint8(uint256(keccak256(abi.encode(block.timestamp, block.difficulty))) % 1000) + 1;
+    }
     
     function mint(string memory _svg) public {
         // does same svg exist already?
@@ -22,5 +30,12 @@ contract Gnft is ERC721Full {
         
         _safeMint(msg.sender, _id);
         _svgExists[_svg] = true;
+
+        // generate features: strength, cost, typ, rarity
+        uint rand = random();
+        strengths.push(rand);
+        costs.push(rand % 2);
+        types.push(rand % 3);
+        rarities.push(rand % 5);
     }
 }
