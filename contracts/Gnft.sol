e3b2f826d712e0ad4e1cdba2a3c78dfce9acd409
David Deket <david.deket@stud.fh-campuswien.ac.at>
2021-08-06 17:17:29 +0200

    buying/ selling added

--- contracts/Gnft.sol
+++ contracts/Gnft.sol
@@ -38,4 +38,28 @@ contract Gnft is ERC721Full {
         types.push(rand % 3);
         rarities.push(rand % 5);
     }
+
+    function allowBuy(uint256 _tokenId, uint256 _price) external {
+        require(msg.sender == ownerOf(_tokenId), 'Not owner of this token');
+        require(_price > 0, 'Price zero');
+        tokenIdToPrice[_tokenId] = _price;
+    }
+
+    function disallowBuy(uint256 _tokenId) external {
+        require(msg.sender == ownerOf(_tokenId), 'Not owner of this token');
+        tokenIdToPrice[_tokenId] = 0;
+    }
+    
+    function buy(uint256 _tokenId) external payable {
+        uint256 price = tokenIdToPrice[_tokenId];
+        require(price > 0, 'This token is not for sale');
+        require(msg.value == price, 'Incorrect value');
+        
+        address seller = ownerOf(_tokenId);
+        _transfer(seller, msg.sender, _tokenId);
+        tokenIdToPrice[_tokenId] = 0; // not for sale anymore
+        payable(seller).transfer(msg.value); // send the ETH to the seller
+
+        emit NftBought(seller, msg.sender, msg.value);
+    }
 }
--- test/Gnft.test.js
+++ test/Gnft.test.js
@@ -71,5 +71,4 @@ contract('Gnft', (accounts) => {
       assert.equal(result.join(','), expected.join(','))
     })
   })
-
 })
